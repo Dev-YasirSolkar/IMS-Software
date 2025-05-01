e9fbc89af411f62d0ecd32fa91aad0cae199222a
Yasir Solkar <mypresentation12911@gmail.com>
2025-05-01 15:31:17 +0530

    Update preload.js

--- electron/preload.js
+++ electron/preload.js
@@ -8,6 +8,6 @@ contextBridge.exposeInMainWorld('electronAPI', {
   saveData: (fileType, data) => ipcRenderer.invoke('save-data', fileType, data),
   selectImage: () => ipcRenderer.invoke('select-image'), // Expose new handler
   copyImageToData: (originalPath) => ipcRenderer.invoke('copy-image-to-data', originalPath), // Expose new handler
-  getImsDataPath: () => ipcRenderer.invoke('get-ims-data-path') // Expose new handler
+  getDataPath: () => ipcRenderer.invoke('get-data-path') // Expose new handler
   // deleteFile: (relativePath) => ipcRenderer.invoke('delete-file', relativePath) // Expose delete handler if implemented
 });
